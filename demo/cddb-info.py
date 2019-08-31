7f6961c0b525131fcd58ead74c0feac05ac6d445
Berthold Höllmann <berthold@xn--hllmanns-n4a.de>
2019-08-31 22:31:55 +0200

    Reformatted code.

--- demo/cddb-info.py
+++ demo/cddb-info.py
@@ -5,13 +5,13 @@ Example of how to use CDDB.py and DiscID.py
 Written 17 Nov 1999 by Ben Gertzfield <che@debian.org
 This work is released under the GNU GPL, version 2 or later.
 """
-from __future__ import (
-division, print_function, absolute_import, unicode_literals)
+from __future__ import division, print_function, absolute_import, unicode_literals
 
 # Standard libraries.
 import os
 import sys
 
+# Third party libraries.
 import CDDB
 import DiscID
 
@@ -38,30 +38,33 @@ def main():
     (query_stat, query_info) = CDDB.query(disc_id)
 
     if query_stat == 200:
-        print("success!\nQuerying CDDB for track info of `%s'... " %
-              query_info['title']),
+        print(
+            "success!\nQuerying CDDB for track info of `%s'... " % query_info["title"]
+        ),
 
         (read_stat, read_info) = CDDB.read(
-            query_info['category'], query_info['disc_id'])
+            query_info["category"], query_info["disc_id"]
+        )
         if read_stat == 210:
             print("success!")
             # Start from 0, not 1
             # thanks to bgp for the fix!
             for i in range(0, disc_id[1]):
-                print("Track %.02d: %s" %
-                      (i + 1, read_info['TTITLE' + repr(i)]))
+                print("Track %.02d: %s" % (i + 1, read_info["TTITLE" + repr(i)]))
         else:
             print("failure getting track info, status: %i" % read_stat)
 
     elif query_stat == 210 or query_stat == 211:
         print("multiple matches found! Matches are:")
         for i in query_info:
-            print("ID: %s Category: %s Title: %s" %
-                  (i['disc_id'], i['category'], i['title']))
+            print(
+                "ID: %s Category: %s Title: %s"
+                % (i["disc_id"], i["category"], i["title"])
+            )
 
     else:
         print("failure getting disc info, status %i" % query_stat)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
